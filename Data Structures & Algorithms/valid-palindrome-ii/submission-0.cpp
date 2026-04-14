01382e931dcf71a2a6a3fe0050afcabfce4e34bb
Bhupendra Singh <bhupendrasingh952891@gmail.com>
2026-04-15 04:16:20 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s, int l, int r) {
+        while (l < r) {
+            if (s[l] != s[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int left = 0, right = s.size() - 1;
+
+        while (left < right) {
+            if (s[left] != s[right]) {
+                // remove one char (either left OR right)
+                return isPalindrome(s, left+1, right) || 
+                       isPalindrome(s, left, right-1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
